b6a56c84951edd5ee21f30cf1c95f5d6a024349c
giulianna2043 <giulianna.2043@gmail.com>
2018-08-06 11:08:11 -0500

    store

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { StoreModule } from "@ngrx/store";
+// import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { TodoComponent } from './todo/todo.component';
-import { TodosListComponent } from './todo/todos-list/todos-list.component';
-import { TodoItemComponent } from './todo/todo-item/todo-item.component';
-import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
-import { TodoAddComponent } from './todo/todo-add/todo-add.component';
-
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { TodoComponent } from "./todo/todo.component";
+import { TodosListComponent } from "./todo/todos-list/todos-list.component";
+import { TodoItemComponent } from "./todo/todo-item/todo-item.component";
+import { TodoFooterComponent } from "./todo/todo-footer/todo-footer.component";
+import { TodoAddComponent } from "./todo/todo-add/todo-add.component";
+import { todoReducer } from "./todo/todo.reducer";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -22,9 +25,14 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
     TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ todos: todoReducer })
+    // StoreDevtoolsModule.instrument({
+    //   maxAge: 25,
+    //   logOnly: environment.production
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
